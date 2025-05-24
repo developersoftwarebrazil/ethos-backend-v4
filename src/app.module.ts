6bddd07235f47e404442b03bb61f653530ab1bd9
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-24 12:18:02 -0300

    migrate ainda funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UserProfileModule } from './user-profile/user-profile.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, UserProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user-profile/user-profile.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserProfileService {
+  create(createUserProfileDto: CreateUserProfileDto) {
+    return 'This action adds a new userProfile';
+  }
+
+  findAll() {
+    return `This action returns all userProfile`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} userProfile`;
+  }
+
+  update(id: number, updateUserProfileDto: UpdateUserProfileDto) {
+    return `This action updates a #${id} userProfile`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} userProfile`;
+  }
+}
