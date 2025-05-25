ca2c0ef51a8fbed0db57369b692ba8b9cac31b1f
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-25 17:59:47 -0300

    rodando localmente

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,31 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL_UNPOOLED ?? process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // Para suportar middlewares ou transações futuras
+  cleanUp() {
+    return this.$disconnect();
   }
 }
