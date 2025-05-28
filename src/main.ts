7d486d1c6bd99b09b1910ec3484d1b907410dbfa
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-28 00:34:56 -0300

    modificado a porta para suportar o railway

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
 
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000, hostname: '0.0.0.0' );
+  // await app.listen(process.env.PORT, hostname: '0.0.0.0');
+  await app.listen(Number(process.env.PORT) || 3000, '0.0.0.0');
+
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
 }
 bootstrap();
