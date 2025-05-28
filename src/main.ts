807c1f099775d89b8e51696d5130c09cfe6aa79d
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-28 11:46:51 -0300

    teste para deploy vercel.v1

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('api');
   // await app.listen(process.env.PORT, hostname: '0.0.0.0');
-  await app.listen(Number(process.env.PORT) || 3000, '0.0.0.0');
+  // await app.listen(Number(process.env.PORT) || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 App running on http://localhost:${port}`);
 
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
 }
