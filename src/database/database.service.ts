c9a22821c434663795412ae2e43b03fde3deb91a
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-06-02 16:16:50 -0300

    melhorado a lógica do dataservice

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -17,15 +17,36 @@ export class DatabaseService
   }
 
   async onModuleInit() {
-    await this.$connect();
+    let connected = false;
+    let retries = 5;
+
+    while (!connected && retries > 0) {
+      try {
+        await this.$connect();
+        connected = true;
+        console.log('✅ Conectado ao banco de dados');
+      } catch (err: unknown) {
+        retries--;
+        console.warn(`🔁 Tentando conectar ao banco... (${5 - retries}/5)`);
+        if (err instanceof Error) {
+          console.error('Erro:', err.message);
+        } else {
+          console.error('Erro desconhecido:', err);
+        }
+        await new Promise((res) => setTimeout(res, 3000));
+      }
+    }
+
+    if (!connected) {
+      throw new Error('❌ Falha ao conectar ao banco após várias tentativas.');
+    }
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
   }
 
-  // Para suportar middlewares ou transações futuras
-  cleanUp() {
-    return this.$disconnect();
+  async cleanUp() {
+    await this.$disconnect();
   }
 }
